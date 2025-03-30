3d840b708cd98b89fdd742e4926ecdc8d08bcb17
chris <chris@mandelics.com>
2025-03-30 14:47:30 -0400

    build.zig.zon: export as module

--- build.zig
+++ build.zig
@@ -2,12 +2,17 @@ const std = @import("std");
 const Target = @import("std").Target;
 const Feature = @import("std").Target.Cpu.Feature;
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const mp_count = b.addExecutable(.{ .name = "mp_count", .root_source_file = b.path("src/mp_count.zig"), .target = target, .optimize = optimize, .single_threaded = true, .strip = true });
     const mp_list = b.addExecutable(.{ .name = "mp_list", .root_source_file = b.path("src/mp_list.zig"), .target = target, .optimize = optimize, .single_threaded = true, .strip = true });
+
+    const module = b.createModule(.{
+        .root_source_file = b.path("src/MultiPermute.zig"),
+    });
+    try b.modules.put(b.dupe("MultiPermute"), module);
 
     b.installArtifact(mp_count);
     b.installArtifact(mp_list);
